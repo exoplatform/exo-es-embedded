4f7c400e11e3e8b1a23da001a4caca71a0e6e120
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-03-22 15:50:00 +0100

    Remove wares plugin

---
+++ war/src/main/java/org.exoplatform.es/EmbeddedESStartupServlet.java
@@ -0,0 +1,59 @@
+package org.exoplatform.es;
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Servlet starting an embedded Elasticsearch node during PLF startup, and stopping it when PLF stops.
+ * It reads configuration from /WEB-INF/elasticsearch.yml.
+ */
+public class EmbeddedESStartupServlet extends HttpServlet {
+
+  private static final Log LOG = ExoLogger.getLogger(EmbeddedESStartupServlet.class);
+
+  protected Node node;
+
+  @Override
+  public void init() throws ServletException {
+
+    LOG.info("Initializing elasticsearch Node '" + getServletName() + "'");
+    ImmutableSettings.Builder settings = ImmutableSettings.settingsBuilder();
+
+    InputStream resourceAsStream = getServletContext().getResourceAsStream("/WEB-INF/elasticsearch.yml");
+    if (resourceAsStream != null) {
+      settings.loadFromStream("/WEB-INF/elasticsearch.yml", resourceAsStream);
+      try {
+        resourceAsStream.close();
+      } catch (IOException e) {
+        // ignore
+      }
+    }
+
+    if (settings.get("http.enabled") == null) {
+      settings.put("http.enabled", false);
+    }
+
+    node = NodeBuilder.nodeBuilder().settings(settings).node();
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+  }
+
+  @Override
+  public void destroy() {
+    if (node != null) {
+      node.close();
+    }
+  }
+}
