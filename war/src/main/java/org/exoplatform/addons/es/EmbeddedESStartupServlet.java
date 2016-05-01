b4216bd03d1f710ea31174ea5b91145a0f055bc4
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-05-01 21:40:31 +0200

    Add Delete By Query plugin

--- war/src/main/java/org/exoplatform/addons/es/EmbeddedESStartupServlet.java
+++ war/src/main/java/org/exoplatform/addons/es/EmbeddedESStartupServlet.java
@@ -5,6 +5,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.mapper.attachments.MapperAttachmentsPlugin;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.plugin.deletebyquery.DeleteByQueryPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -14,6 +15,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 
 /**
@@ -48,7 +51,9 @@ public class EmbeddedESStartupServlet extends HttpServlet {
 
     // use the custom EmbeddedNode class instead of Node directly to be able to load plugins from classpath
     Environment environment = new Environment(settings.build());
-    node = new EmbeddedNode(environment, Version.CURRENT, Collections.<Class<? extends Plugin>>singletonList(MapperAttachmentsPlugin.class));
+    Collection plugins = new ArrayList<>();
+    Collections.<Class<? extends Plugin>>addAll(plugins, MapperAttachmentsPlugin.class, DeleteByQueryPlugin.class);
+    node = new EmbeddedNode(environment, Version.CURRENT, plugins);
     node.start();
   }
 
