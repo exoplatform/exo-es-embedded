de5a5568b6b097d3841aa8771836f4011358142d
Boubaker Khanfir <bkhanfir@exoplatform.com>
2017-03-02 22:04:25 +0100

    PLF-7093 Upgrade to ES 5.2.2

--- war/src/main/java/org/exoplatform/addons/es/EmbeddedESStartupServlet.java
+++ war/src/main/java/org/exoplatform/addons/es/EmbeddedESStartupServlet.java
@@ -1,23 +1,27 @@
 package org.exoplatform.addons.es;
 
-import org.elasticsearch.Version;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.mapper.attachments.MapperAttachmentsPlugin;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.plugin.deletebyquery.DeleteByQueryPlugin;
-import org.elasticsearch.plugins.Plugin;
-import org.exoplatform.commons.utils.PropertyManager;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.reindex.ReindexPlugin;
+import org.elasticsearch.ingest.attachment.IngestAttachmentPlugin;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeValidationException;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.Netty4Plugin;
+
+import org.exoplatform.commons.utils.PropertyManager;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 
 /**
  * Servlet starting an embedded Elasticsearch node during PLF startup, and stopping it when PLF stops.
@@ -42,12 +46,12 @@ public class EmbeddedESStartupServlet extends HttpServlet {
     }
 
     LOG.info("Initializing elasticsearch Node '" + getServletName() + "'");
-    Settings.Builder settings = Settings.settingsBuilder();
+    Settings.Builder settings = Settings.builder();
 
     InputStream resourceAsStream = getServletContext().getResourceAsStream("/WEB-INF/elasticsearch.yml");
     if (resourceAsStream != null) {
-      settings.loadFromStream("/WEB-INF/elasticsearch.yml", resourceAsStream);
       try {
+        settings.loadFromStream("/WEB-INF/elasticsearch.yml", resourceAsStream);
         resourceAsStream.close();
       } catch (IOException e) {
         // ignore
@@ -58,11 +62,26 @@ public class EmbeddedESStartupServlet extends HttpServlet {
       settings.put("http.enabled", false);
     }
 
+    String pathData = settings.get("path.data");
+    if (pathData == null || pathData.startsWith("${")) {
+      if (pathData == null) {
+        settings.put("path.data", System.getProperty("exo.data.dir"));
+      } else {
+        pathData = pathData.replace("${", "");
+        pathData = pathData.replace("}", "");
+        settings.put("path.data", System.getProperty(pathData));
+      }
+    }
+
     // use the custom EmbeddedNode class instead of Node directly to be able to load plugins from classpath
     Collection plugins = new ArrayList<>();
-    Collections.<Class<? extends Plugin>>addAll(plugins, MapperAttachmentsPlugin.class, DeleteByQueryPlugin.class);
+    Collections.<Class<? extends Plugin>>addAll(plugins, Netty4Plugin.class, IngestAttachmentPlugin.class, ReindexPlugin.class);
     node = new EmbeddedNode(settings.build(), Version.CURRENT, plugins);
-    node.start();
+    try {
+      node.start();
+    } catch (NodeValidationException e) {
+      LOG.error("Error when starting ES in embedded mode", e);
+    }
   }
 
   @Override
@@ -73,7 +92,11 @@ public class EmbeddedESStartupServlet extends HttpServlet {
   @Override
   public void destroy() {
     if (node != null) {
-      node.close();
+      try {
+        node.close();
+      } catch (IOException e) {
+        LOG.error("Error when stopping ES in embedded mode", e);
+      }
     }
   }
 }
